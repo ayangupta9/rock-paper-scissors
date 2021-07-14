aa1b12ce7caf8d24d94366206291fa531320354e
ayangupta9 <ayangupta1000@gmail.com>
2021-07-14 22:52:28 +0530

    First commit RPS

---
+++ app.js
@@ -0,0 +1,15 @@
+// // const choice = document.querySelector('.innerChoiceWrapper')
+// const choices = document.getElementsByClassName('choice')
+// const choicesP = document.getElementsByClassName('choiceP')
+
+// for (let i = 0; i < choices.length; i++) {
+//   choices[i].addEventListener('mouseenter', e => {
+//     e.target.style.transform = 'scale(1.15)'
+//     e.target.style.transition = 'ease 0.1s'
+//     e.target.style.cursor = 'pointer'
+//   })
+//   choices[i].addEventListener('mouseleave', e => {
+//     e.target.style.transform = 'scale(1.0)'
+//     e.target.style.transition = 'ease 0.1s'
+//   })
+// }
